5a7e3affe04585eff839d6f8694777e6b4b08b16
Preeti Palak Kaur <75083231+preeti1605@users.noreply.github.com>
2026-01-04 01:21:50 +0530

    Update project1.py
    commented one line of code

--- BeginnerProjects/project1.py
+++ BeginnerProjects/project1.py
@@ -10,7 +10,7 @@ for file in folder.iterdir():
 
     #print(file.suffix)
     new_name = f"automation_{c}{file.suffix}"
-    print(file, "->", new_name)
+    #print(file, "->", new_name)
     file.rename(file.with_name(new_name))
     print("Renamed:", new_name)
     c+=1
