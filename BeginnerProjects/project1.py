91decaa2e4d7da7ff34ed6194db1ac303a8630e6
Preeti Palak Kaur <75083231+preeti1605@users.noreply.github.com>
2026-01-04 01:19:23 +0530

    Added the python file
    Added the python file for my first automation project. Bulk File Renamer

---
+++ BeginnerProjects/project1.py
@@ -0,0 +1,16 @@
+
+
+from pathlib import Path
+folder = Path("/AutomationProjects/BeginnerProjects/BulkFileRenamer/AutomationDir")
+
+c=1
+for file in folder.iterdir():
+    #print(c,file)
+    #c+=1
+
+    #print(file.suffix)
+    new_name = f"automation_{c}{file.suffix}"
+    print(file, "->", new_name)
+    file.rename(file.with_name(new_name))
+    print("Renamed:", new_name)
+    c+=1
